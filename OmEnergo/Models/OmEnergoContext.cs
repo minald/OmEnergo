2b61829b5243343af5f3d9540c9cd0eba5205a3a
minaldandrey <minaldandrey@gmail.com>
2018-02-23 00:21:57 +0300

    Implementing Product and Stabilizer

--- OmEnergo/Models/OmEnergoContext.cs
+++ OmEnergo/Models/OmEnergoContext.cs
@@ -4,6 +4,8 @@ namespace OmEnergo.Models
 {
     public class OmEnergoContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
         public OmEnergoContext(DbContextOptions<OmEnergoContext> options) : base(options)
         {
 
