e079110eb2451e137778c571147190206e637534
minaldandrey <minaldandrey@gmail.com>
2018-02-24 21:38:11 +0300

    Implementing Stabilizer and improving Product

--- OmEnergo/Models/Product.cs
+++ OmEnergo/Models/Product.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace OmEnergo.Models
 {
     public class Product
     {
+        [Key]
+        public int Id { get; set; }
 
+        public string Name { get; set; }
+
+        public string Description { get; set; }
     }
 }
