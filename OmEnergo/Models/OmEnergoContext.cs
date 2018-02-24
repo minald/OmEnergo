fc342bacd31351934a10786a6577bee01c57d9aa
minaldandrey <minaldandrey@gmail.com>
2018-02-24 21:14:27 +0300

    Revert "Implementing Product and Stabilizer"
    This reverts commit 2b61829b5243343af5f3d9540c9cd0eba5205a3a.

--- OmEnergo/Models/OmEnergoContext.cs
+++ OmEnergo/Models/OmEnergoContext.cs
@@ -4,8 +4,6 @@ namespace OmEnergo.Models
 {
     public class OmEnergoContext : DbContext
     {
-        public DbSet<Product> Products { get; set; }
-
         public OmEnergoContext(DbContextOptions<OmEnergoContext> options) : base(options)
         {
 
--- OmEnergo/Models/Product.cs
+++ OmEnergo/Models/Product.cs
@@ -1,14 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OmEnergo.Models
 {
     public class Product
     {
-        [Key]
-        public int Id { get; set; }
 
-        public string Name { get; set; }
-
-        public string Description { get; set; }
     }
 }
