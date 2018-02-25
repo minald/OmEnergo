8693024af69d7fa46c3dbd14200b5d5120d4ae99
minaldandrey <minaldandrey@gmail.com>
2018-02-25 10:58:12 +0300

    Adding one more new field to Stabilizer

--- OmEnergo/Models/Stabilizer.cs
+++ OmEnergo/Models/Stabilizer.cs
@@ -33,6 +33,9 @@ namespace OmEnergo.Models
 
         public bool ShortCircuitProtection { get; set; }
 
+        public bool VoltageSurgesProtection { get; set; }
+
+
         //Some more properties... 
     }
 }
