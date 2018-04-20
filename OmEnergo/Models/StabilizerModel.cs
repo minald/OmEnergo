b478a63ce6505938e8dacde44e577bba29f0c282
stasOR4ik <kov-stas99@mail.ru>
2018-04-20 23:18:15 +0300

    5 - Implement good-looking price styles (Adding price styles and adding 2 methods in StabilizerModel.cs)

--- OmEnergo/Models/StabilizerModel.cs
+++ OmEnergo/Models/StabilizerModel.cs
@@ -26,5 +26,19 @@ namespace OmEnergo.Models
             var stabilizerSeries = Stabilizer.Series.Replace('"', '\'');
             return $"/images/{Stabilizer.Product.EnglishName}/{stabilizerSeries}/{Name.Replace('/', '-')}.jpg";
         }
+
+        public int GetPriceIntegerPart() => (int)Price;
+
+        public string GetStringPriceFractionalPart()
+        {
+            if ((Price - GetPriceIntegerPart()) * 100 < 10)
+            {
+                return "0" + ((int)((Price - GetPriceIntegerPart()) * 100)).ToString();
+            }
+            else
+            {
+                return ((int)((Price - GetPriceIntegerPart()) * 100)).ToString();
+            }
+        }
     }
 }
