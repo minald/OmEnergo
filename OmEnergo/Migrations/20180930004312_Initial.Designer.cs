9673c53e6487f04ad187cb79740042cf3b7ad617
PC\Andrey <minaldandrey@gmail.com>
2018-09-30 03:46:53 +0300

    Removing useless classes

--- OmEnergo/Migrations/20180930004312_Initial.Designer.cs
+++ OmEnergo/Migrations/20180930004312_Initial.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace OmEnergo.Migrations
 {
     [DbContext(typeof(OmEnergoContext))]
-    partial class OmEnergoContextModelSnapshot : ModelSnapshot
+    [Migration("20180930004312_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- OmEnergo/Models/OmEnergoContext.cs
+++ OmEnergo/Models/OmEnergoContext.cs
@@ -10,18 +10,6 @@ namespace OmEnergo.Models
 
         public DbSet<ProductModel> ProductModels { get; set; }
 
-        public DbSet<Stabilizer> Stabilizers { get; set; }
-
-        public DbSet<StabilizerModel> StabilizerModels { get; set; }
-
-        public DbSet<Inverter> Inverters { get; set; }
-
-        public DbSet<InverterModel> InverterModels { get; set; }
-
-        public DbSet<Autotransformer> Autotransformers { get; set; }
-
-        public DbSet<AutotransformerModel> AutotransformerModels { get; set; }
-
         public OmEnergoContext() { }
 
         public OmEnergoContext(DbContextOptions<OmEnergoContext> options) : base(options) { }
