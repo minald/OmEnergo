a15bccac5e8283f9dcc78e27ae83fc2d8709d02f
AndreyMinald <minaldandrey@gmail.com>
2019-06-25 02:46:21 +0300

    Implementing the SidebarLink tag helper

---
+++ OmEnergo/Infrastucture/TagHelpers/SidebarLinkTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace OmEnergo.Infrastucture.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "om-children-amount")]
+    public class SidebarLinkTagHelper : TagHelper
+    {
+        public int OmChildrenAmount { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (OmChildrenAmount != 0)
+            {
+                output.Attributes.SetAttribute("class", "fas fa-angle-right");
+            }
+        }
+    }
+}
