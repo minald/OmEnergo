f1fd88b8ae7fc905f7e8b27014cd40d5faa505e3
AndreyMinald <minaldandrey@gmail.com>
2019-07-01 11:14:43 +0300

    Creating the OmEnergo.Tests project

---
+++ OmEnergo.Tests/Infrastructure/FileManagerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OmEnergo.Infrastructure;
+using Xunit;
+
+namespace OmEnergo.Tests.Infrastructure
+{
+    public class FileManagerTests
+    {
+        public FileManagerTests()
+        {
+
+        }
+
+        [Fact]
+        public void method()
+        {
+
+        }
+    }
+}
