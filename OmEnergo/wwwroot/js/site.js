9a35acfbe1fe2655aa130c0849809f00fef0aec8
andreyminald <minaldandrey@gmail.com>
2019-10-28 18:49:29 +0300

    Embedding of the lightslider and lightgallery functionality

--- OmEnergo/wwwroot/js/site.js
+++ OmEnergo/wwwroot/js/site.js
@@ -4,8 +4,33 @@
 
 'use strict';
 
+//For the navigation tree sidebar
 $('.fa-angle-right').click(function () {
-	$(this).toggleClass('fa-angle-right');
-	$(this).toggleClass('fa-angle-down');
+    $(this).toggleClass('fa-angle-right');
+    $(this).toggleClass('fa-angle-down');
 });
+
+//The Jivosite widget
+(function () {
+    var widget_id = 'r8B1qiUPl0';
+    var d = document;
+    var w = window;
+    function l() {
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = '//code.jivosite.com/script/widget/' + widget_id;
+        var ss = document.getElementsByTagName('script')[0];
+        ss.parentNode.insertBefore(s, ss);
+    }
+    if (d.readyState === 'complete') {
+        l();
+    } else {
+        if (w.attachEvent) {
+            w.attachEvent('onload', l);
+        } else {
+            w.addEventListener('load', l, false);
+        }
+    }
+})();
 
