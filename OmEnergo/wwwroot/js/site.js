fb43858516964d87f2191c49f8fefd0d9423c996
andreyminald <minaldandrey@gmail.com>
2020-10-13 04:17:30 +0300

    Replacing all tabs to spaces in json and wwwroot files

--- OmEnergo/wwwroot/js/site.js
+++ OmEnergo/wwwroot/js/site.js
@@ -6,38 +6,38 @@
 
 // For the navigation tree sidebar
 $('.fa-angle-right').click(function () {
-    $(this).toggleClass('fa-angle-right');
-    $(this).toggleClass('fa-angle-down');
+	$(this).toggleClass('fa-angle-right');
+	$(this).toggleClass('fa-angle-down');
 });
 
 // The Jivosite widget
 (function () {
-    var widget_id = 'r8B1qiUPl0';
-    var d = document;
-    var w = window;
-    function l() {
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = '//code.jivosite.com/script/widget/' + widget_id;
-        var ss = document.getElementsByTagName('script')[0];
-        ss.parentNode.insertBefore(s, ss);
-    }
-    if (d.readyState === 'complete') {
-        l();
-    } else {
-        if (w.attachEvent) {
-            w.attachEvent('onload', l);
-        } else {
-            w.addEventListener('load', l, false);
-        }
-    }
+	var widget_id = '';
+	var d = document;
+	var w = window;
+	function l() {
+		var s = document.createElement('script');
+		s.type = 'text/javascript';
+		s.async = true;
+		s.src = '//code.jivosite.com/script/widget/' + widget_id;
+		var ss = document.getElementsByTagName('script')[0];
+		ss.parentNode.insertBefore(s, ss);
+	}
+	if (d.readyState === 'complete') {
+		l();
+	} else {
+		if (w.attachEvent) {
+			w.attachEvent('onload', l);
+		} else {
+			w.addEventListener('load', l, false);
+		}
+	}
 })();
 
 // For the _UploadFileButton.cshtml
 // This code is here, because this code doesn't work inside the Scripts section inside the _UploadFileButton.cshtml partial view.
 $(".custom-file-input").on("change", function () {
-    var fileName = $(this).val().split("\\").pop();
-    $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+	var fileName = $(this).val().split("\\").pop();
+	$(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
 
